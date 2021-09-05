16927184eb3c8263ef21619d2581e71637248c88
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2021-09-05 20:36:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const cats = [
     }
 ];
 
-// var hello = "hello"
+//javascript MA1
 
 // //Question 1
 
@@ -67,7 +67,7 @@ resultsContainer.style.background = "yellow";
 
 function findCats (list){
     for(let i = 0; i < list.length; i++){
-        // console.log(list[i].name);
+        console.log(list[i].name);
     }
 }
 
@@ -105,61 +105,3 @@ console.log(newHtml);
 const container = document.querySelector(".cat-container");
 
 container.innerHTML = newHtml;
-
-// this is wrong, rewrite code tomorrow
-
-// const container = document.querySelector(".cat-container");
-
-// console.log();
-
-// let html = "";
-
-// function createCats(){
-//     for(let i = 0; i < cats.length; i++){
-
-//         let age = "Age unknown";
-
-//         if(cats[i].age){
-//             age = cats[i].age;
-//         }
-//         html += `<div>
-//                     <h5>${cats[i].name}</h5>
-//                     <p>${age}</p>
-        
-//                 </div>
-//         `;
-//     }
-//     container.innerHTML = html;
-//     console.log(html);
-// }
-
-
-
-// createCats();
-
-
-
-
-
-
-// function createCats(){
-//     let html =""
-//     for(let i = 0; i < cats.length; i++){
-//         let ageNot = "Uknown age";
-//         if (cats[i].age) {
-//             catsNot = cats[i].age;
-
-//         }
-//         html += `<div>
-//             <h5>${cats[i].name}</h5>
-//             <p>Age: ${cats[i].age}</p>
-//         </div>
-//         `;
-//     }
-//     return html;
-// }
-// createCats();
-
-
- // kittens = kittens + "<h5>" + cats[i].name + cats[i].age +  "</h5>";
-        // console.log(kittens);
